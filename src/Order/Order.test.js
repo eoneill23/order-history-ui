ffb86d8d8d510ab2c0d2d8347381daaba6b599aa
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-29 11:21:41 -0600

    Get Order snapshot and button tests working

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -4,13 +4,24 @@ import { shallow } from 'enzyme';
 import Order from './Order.js';
 
 describe('Order', () => {
-  let wrapper;
+  let wrapper, deleteOrderMock;
 
   beforeEach(() => {
-    wrapper = shallow(<Order />)
+    deleteOrderMock = jest.fn();
+    wrapper = shallow(<Order id={1} img={'twitter.com'} name={'stereo'} description={'cool stereo'} price={'200'} deleteOrder={deleteOrderMock}/>)
   });
 
   it('should render without crashing', () => {
     expect(wrapper.length).toEqual(1)
+  });
+
+  it('should match the snapshot with all data passed in correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should fire the deleteOrder method when the button is clicked', () => {
+    wrapper.find('button').simulate('click');
+
+    expect(deleteOrderMock).toHaveBeenCalled();
   });
 });
