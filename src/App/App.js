d95275d574527d076fcc6db69d1f56a033d59763
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-29 09:17:49 -0600

    Build out modular file structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import OrderList from '../OrderList/OrderList.js';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      
+      orders: []
     }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    
   }
 }
 
