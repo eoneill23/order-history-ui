d78e23dba1748c4d0dda89a44c2942eb7fbb32cc
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-29 11:29:44 -0600

    Get OrderList tested and add apiCalls.test.js file

--- src/OrderList/OrderList.test.js
+++ src/OrderList/OrderList.test.js
@@ -4,13 +4,24 @@ import { shallow } from 'enzyme';
 import OrderList from './OrderList.js';
 
 describe('OrderList', () => {
-  let wrapper;
+  let wrapper, deleteOrderMock;
 
   beforeEach(() => {
-    wrapper = shallow(<OrderList orders={[]}/>)
+    deleteOrderMock = jest.fn();
+    wrapper = shallow(<OrderList orders={
+      [
+        { id: 1, img: 'google.com', name: 'Television', price: '200' },
+        { id: 2, img: 'facebook.com', name: 'Radio', price: '100' }
+      ]
+    }
+    deleteOrder={deleteOrderMock}/>)
   });
 
   it('should render without crashing', () => {
     expect(wrapper.length).toEqual(1)
+  });
+
+  it('should match the snapshot with all data passed in correctly', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
