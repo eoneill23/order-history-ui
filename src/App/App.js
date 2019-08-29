42b1d1a471905bbcda46a057786903ea869a89cf
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-29 09:44:16 -0600

    Fetch data and display on the DOM

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import OrderList from '../OrderList/OrderList.js';
+// import { fetchOrders } from '../util/apiCalls.js';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      orders: []
+      orders: [],
+      err: ''
     }
   }
 
   componentDidMount() {
-
+    fetch('http://localhost:3001/api/v1/purchases')
+    .then(res => res.json())
+    .then(data => this.setState({orders: data}))
+    .catch(err => {
+      this.setState({err: 'There was an issue getting your orders.'})
+    })
   }
 
   render() {
---
+++ src/util/apiCalls.js
@@ -0,0 +1,12 @@
+export const fetchOrders = () => {
+  fetch('http://localhost:3001/api/v1/purchases')
+  .then(response => {
+    if(!response.ok) {
+      throw Error ('There was an error getting your orders.');
+    }
+    return response.json();
+  })
+  .catch(error => {
+    throw Error(error.message)
+  })
+}
