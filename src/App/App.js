2e550672e9a6e366f5523d6041cc623df1d29056
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-29 09:13:43 -0600

    Initial commit

--- src/App/App.js
+++ src/App/App.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <h1 className='app-title'>My Order History</h1>
-          <div className='purchase-form'>
-
-          </div>
-        </header>
-        <div className='purchase-container'>
-
-        </div>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      
+    }
   }
 }
 
