99e058f145a497075b3e7b6a8702cfe3e08282be
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-29 10:54:07 -0600

    Build out basic test structure

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+ let wrapper, mockOrder;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+    mockOrder = {id: 1, img: 'google.com', name: 'Television', price: '200'}
+  });
+
+  it('should render without crashing', () => {
+    expect(wrapper.length).toBe(1)
+  });
+
+  it('should add an order when addOrder is invoked', () => {
+    const expected = [mockOrder]
+    expect(wrapper.state('orders')).toEqual([]);
+
+    wrapper.instance().addOrder(mockOrder);
+
+    expect(wrapper.state('orders')).toEqual(expected)
+  });
 });
+
---
+++ src/Order/Order.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import Order from './Order.js';
