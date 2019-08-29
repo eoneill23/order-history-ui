c89ef76eb185669c5c94674e3cba231f2f86aef5
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-29 11:57:25 -0600

    Delete unused variable from App

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,6 @@ import OrderList from '../OrderList/OrderList.js';
 import OrderForm from '../OrderForm/OrderForm.js';
 import { fetchOrders, addOrder, removeOrder } from '../util/apiCalls.js';
 import './App.css';
-import Order from '../Order/Order.js';
 
 class App extends Component {
   constructor() {
